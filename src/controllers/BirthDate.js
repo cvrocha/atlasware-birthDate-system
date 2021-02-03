92b0970f191e1f60b00f4a67406cc6ed7ea30ac8
Jorge H Baptista <jorgehenrique@live.com>
2021-02-03 08:40:09 -0300

    Route BirthDate and install moment lib

---
+++ src/controllers/BirthDate.js
@@ -0,0 +1,23 @@
+/* eslint-disable eqeqeq */
+const moment = require('moment');
+const db = require('../models');
+
+const Employee = db.employee;
+
+const BirthDateController = {
+  async index(req, res) {
+    await Employee.findAll({
+      where: {
+        birthDate: {
+          $gte: moment().toDate(),
+        },
+      },
+    }).then((data) => {
+      res.status(200).send(data);
+    }).catch((err) => {
+      res.status(500).send({ message: err.message || 'Algo de errado aconteceu, tente novamente' });
+    });
+  },
+};
+
+module.exports = BirthDateController;
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const authController = require('./controllers/Auth');
 const employeeController = require('./controllers/Employee');
+const birthDateController = require('./controllers/BirthDate');
 
 const authMiddleware = require('./middlewares/Auth');
 
@@ -14,5 +15,7 @@ routes.post('/employee', authMiddleware, employeeController.store);
 routes.get('/employee/:id', authMiddleware, employeeController.show);
 routes.put('/employee/:id', authMiddleware, employeeController.edit);
 routes.delete('/employee/:id', authMiddleware, employeeController.destroy);
+// Aniversariantes
+routes.get('/birthdate', authMiddleware, birthDateController.index);
 
 module.exports = routes;
