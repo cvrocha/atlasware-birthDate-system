3ec36a0175e787d4dae298b1a3e1f255b21aa2cf
Jorge H Baptista <jorgehenrique@live.com>
2021-02-03 08:10:12 -0300

    Insert Route Delete / Put / Show

--- src/controllers/Employee.js
+++ src/controllers/Employee.js
@@ -1,3 +1,4 @@
+/* eslint-disable eqeqeq */
 const db = require('../models');
 
 const Employee = db.employee;
@@ -5,6 +6,14 @@ const Employee = db.employee;
 const EmployeeController = {
   async index(req, res) {
     await Employee.findAll().then((data) => {
+      res.status(200).send(data);
+    }).catch((err) => {
+      res.status(500).send({ message: err.message || 'Algo de errado aconteceu, tente novamente' });
+    });
+  },
+  async show(req, res) {
+    const { id } = req.params;
+    await Employee.findByPk(id).then((data) => {
       res.status(200).send(data);
     }).catch((err) => {
       res.status(500).send({ message: err.message || 'Algo de errado aconteceu, tente novamente' });
@@ -19,7 +28,7 @@ const EmployeeController = {
       res.status(400).send({ message: 'Atributos obrigatorios não podem estar em branco' });
     }
 
-    Employee.create({
+    await Employee.create({
       firstName, lastName, email, phone, comments, birthDate,
     }).then((data) => {
       res.status(201).send(data);
@@ -27,6 +36,47 @@ const EmployeeController = {
       res.status(500).send({
         message: err.message || 'Algo de errado aconteceu',
       });
+    });
+  },
+  async edit(req, res) {
+    const {
+      firstName, lastName, birthDate,
+    } = req.body;
+    const { id } = req.params;
+
+    if (!firstName || !lastName || !birthDate) {
+      res.status(400).send({ message: 'Atributos obrigatorios não podem estar em branco' });
+    }
+    await Employee.update(req.body, {
+      where: { id },
+    }).then((num) => {
+      if (num == 1) {
+        res.send({
+          message: 'Empregado alterado com sucesso',
+        });
+      } else {
+        res.send({
+          message: `Não foi possivel encontrar o id=${id}.`,
+        });
+      }
+    });
+  },
+  async destroy(req, res) {
+    const { id } = req.params;
+    await Employee.destroy({
+      where: { id },
+    }).then((data) => {
+      if (data == 1) {
+        res.status(204).send({
+          message: 'Funcionario deletado com successo!',
+        });
+      } else {
+        res.status(404).send({
+          message: `Não foi possivel localizar o funcionario com id = ${id}.`,
+        });
+      }
+    }).catch((err) => {
+      res.status(500).send({ message: err.message || 'Algo de errado aconteceu, tente novamente' });
     });
   },
 };
--- src/routes.js
+++ src/routes.js
@@ -11,5 +11,8 @@ routes.post('/register', authController.store);
 
 routes.get('/employee', authMiddleware, employeeController.index);
 routes.post('/employee', authMiddleware, employeeController.store);
+routes.get('/employee/:id', authMiddleware, employeeController.show);
+routes.put('/employee/:id', authMiddleware, employeeController.edit);
+routes.delete('/employee/:id', authMiddleware, employeeController.destroy);
 
 module.exports = routes;
